ccda049aa463b469710a351b5869bc8d8644025f
Tejas Ghadge <ghadgetejas802@gmail.com>
2025-01-19 12:47:48 +0530

    Create chatbot.py
    Age part complete

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+print("By TEJAS\n")
+print("Hello Frnds")
+
+name = "TG Chatbot"
+print(f"chatBot: Hey, I am {name}")
+loop = True
+
+while(looo):
+    useName = input("chatBot: What is your name\t").capitalize()
+
+age = 2
+def botAge(age):
+    print("chatBot: I am",age,"year old")
+    return age
+  
+botAge(age)
+
+userAge = int(input(f"chatBot: {useName}, What about you:\t"))
+
+
