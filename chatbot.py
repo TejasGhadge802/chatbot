affeeea662b6b82e7b9e2bbea64dd1e8df272c81
Tejas Ghadge <ghadgetejas802@gmail.com>
2025-01-29 20:17:03 +0530

    Update chatbot.py
    Complete.
    There will be some update in program.

--- chatbot.py
+++ chatbot.py
@@ -3,18 +3,46 @@ print("Hello Frnds")
 
 name = "TG Chatbot"
 print(f"chatBot: Hey, I am {name}")
-loop = True
 
-while(looo):
+while(True):
     useName = input("chatBot: What is your name\t").capitalize()
 
-age = 2
-def botAge(age):
-    print("chatBot: I am",age,"year old")
-    return age
-  
-botAge(age)
+    #Bot age function
+    age = 2
+    def botAge(age):
+        print("chatBot: I am",age,"year old")
+    botAge(age) # call
 
-userAge = int(input(f"chatBot: {useName}, What about you:\t"))
+    userAge = int(input(f"chatBot: {useName}, What about you:\t"))
 
+    # Age different function
+    def ageDiff(a,b):
+        diff = a-b;
+        if(diff > b):
+            print(f'chatbot: {userName} you are {diff} year older then me')
+        elif(diff == 0):
+            print(f'chatbot: ooh!, {userName} we both are 2 year old')
+        else:
+            print(f'chatbot: ooh!, {userName} you are {abs(diff)} year younger then me')
+    ageDiff(userAge,age) #call
+
+    
+    # Userid
+    print (f"chatBot; Hey, (useName) can I suggest a user ID for you")
+    responce = input("chatBot; yes/no: \t").lower()
+    
+    if(responce == "yes"):
+        resYes = input("Enter your full name without space: \t")
+        userld = len(res Yes)
+        print("Your userld is:",useName.lower()+"_"+str(userld))
+
+    # restart
+    print (f"chatBot; Hey, (useName) Do you want to try again!")
+    res = input("chatBot; yes/no: \t").lower()
+    if(res == 'no'):
+        break
+
+print("exiting...")
+print("Thanks, for interaction")
+    
 
